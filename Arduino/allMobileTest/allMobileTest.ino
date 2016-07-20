890acb8e42f58e6622f71ae4e6dce24d51ed5739
nikyarora <nikyarora@gmail.com>
2016-07-19 18:15:19 -0700

    fixing the broadcast

--- Arduino/allMobileTest/allMobileTest.ino
+++ Arduino/allMobileTest/allMobileTest.ino
@@ -144,10 +144,12 @@ void loop()
         {
             if(manager.sendtoWait(data, sizeof(data), from))
             {
+              Serial.println("hello yay");
               sendtoWait = 1;
             }
             else
             {
+              Serial.println("hello");
               sendtoWait = 0;
             }
         }
@@ -456,44 +458,65 @@ void broadcast()
   {
     currentNodeBroadcasting++;
   }
-  //BROADCAST
-  Serial.println();
-  Serial.println("Broadcasting ID to all receiving nodes.");
-  uint8_t waitToReceive = 0;
-  int numAcks = 0;
 
-    //Broadcast the message to all other reachable nodes.
-  if (manager.sendtoWait(data, sizeof(data), RH_BROADCAST_ADDRESS))
+  uint8_t recvAckFromAllNodes = 0;
+  while(recvAckFromAllNodes == 0)
   {
-    Serial.println("Broadcast Successful");
-    Serial.print("My Address: ");
-    Serial.println(CLIENT_ADDRESS);
-    
-    while(waitToReceive == 0 && numAcks != NUMBER_OF_NODES - 1) 
+    //BROADCAST
+    Serial.println();
+    Serial.println("Broadcasting ID to all receiving nodes.");
+  
+    //Broadcast the message to all other reachable nodes.
+    if (manager.sendtoWait(data, sizeof(data), RH_BROADCAST_ADDRESS))
     {
-     if (manager.available())
-     {
-        Serial.println("manager is available");
-        // Wait for a message addressed to us from the client
-        uint8_t len = sizeof(buf);
-        uint8_t from;
-        uint8_t to;
-        int8_t rssi;
-        if (manager.recvfromAck(buf, &len, &from, &to))
-        {
-          waitToReceive = 1;
-          numAcks++;
-        }
-        else
-        {
-          waitToReceive = 0;
-        }
-     }
-     
+      Serial.println("Broadcast Successful");
+      Serial.print("My Address: ");
+      Serial.println(CLIENT_ADDRESS);
+    }
+    else
+      Serial.println("sendtoWait failed");
+
+    if (manager.available())
+    {
+      // Wait for a message addressed to us from the client
+      uint8_t len = sizeof(buf);
+      uint8_t from;
+      uint8_t to;
+      int8_t rssi;
+      if (manager.recvfromAck(buf, &len, &from, &to))
+      {
+          switch (from) 
+          {
+            case NODE_2_ADDRESS:
+            Serial.println("Received From M2");
+            rssiReceiptFlags [0] = 1;
+            break;
+            
+            case NODE_3_ADDRESS:
+            Serial.println("Received From M3");  
+            rssiReceiptFlags [1] = 1;
+            break;
+          }
+      }
+      
+      uint8_t allAckRecv=1;
+      for (int i=0; i<NUMBER_OF_NODES-1; i++)
+      {
+        allAckRecv = allAckRecv && rssiReceiptFlags[i];
+      }
+
+      //Checks to see if all the data is received. If so, sends out a broadcast signal
+      if(allAckRecv)
+      {    
+        recvAckFromAllNodes = 1;
+      }
+      else
+      {
+        recvAckFromAllNodes = 0;
+      }
+
     }
   }
-  else
-    Serial.println("sendtoWait failed");
 }
 
 
