621ca8ea3ebffcb260ca8971a522331720dad7d6
nikyarora <nikyarora@gmail.com>
2016-07-29 12:20:55 -0700

    testing

--- Arduino/ssLocalizationLib/ssLocalizationLib.ino
+++ Arduino/ssLocalizationLib/ssLocalizationLib.ino
@@ -2,22 +2,38 @@
 #include <math.h>
 
 #define num_nodes 2
+#define xsize 3
 
 void setup() 
 {
+  Serial.begin(9600);
+  
+  char x[xsize] = {2, 3, 4};
+  char y[xsize] = {1, 2, 3};
+  char cal[2][2] = {
+    {1, 2},
+    {3, 4}
+  };
+  double xi = 2.0;
+  double yi = 3.0;
+  double A[xsize][2];
+  double B[xsize];
+  double C[xsize][1];
+  generateMatrices(x, y, cal, xi, yi, A, B, C);
+
+  //Serial.println(A);
+  //Serial.println(B);
+  //Serial.println(C);
 }
 
 void loop() 
 {
 }
 
-void generateMatrices(char *ptr, char *ptr2, char **ptr3cal, double xi, double yi, double A[num_nodes][2], double B[num_nodes],double C[num_nodes][1])
+void generateMatrices(char *ptrx, char *ptry, char **ptrcal, double xi, double yi, double A[num_nodes][2], double B[num_nodes],double C[num_nodes][1])
 {
   int m = num_nodes;
   int n = 2;
- // double A[m][n];
-  //double B[m];
-  //double C[m][1];
   for(int i = 0; i < m; i++)
   {
     for(int j = 0; j < n; j++)
@@ -35,8 +51,8 @@ void generateMatrices(char *ptr, char *ptr2, char **ptr3cal, double xi, double y
   {
       for(int j = 0; j < n; j++)
       {
-        xnew[m*j + i] = ptr[n*i + j];
-        ynew[m*j + i] = ptr2[n*i + j];
+        xnew[m*j + i] = ptrx[n*i + j];
+        ynew[m*j + i] = ptry[n*i + j];
       }
   }
 
@@ -45,10 +61,10 @@ void generateMatrices(char *ptr, char *ptr2, char **ptr3cal, double xi, double y
     double linearM[] = {2*(xi-xnew[i])/(pow(xi-xnew[i],2)+pow(yi-ynew[i],2)), 2*(yi-ynew[i])/(pow(xi-xnew[i],2)+pow(yi-ynew[i],2))};
     for(int j = 0; j < n; j++)
     {
-      A[i][j] = -10.0 /ptr3cal[i][0]* linearM[i];
+      A[i][j] = -10.0 /ptrcal[i][0]* linearM[i];
     }
-    C[i][0] = (-ptr3cal[i][1]-25.2-20 * log10(sqrt(pow(xi-xnew[i],2) + pow(yi-ynew[i],2))) / ptr3cal[i][0]);
-    B[i] = 1/ptr3cal[i][0];
+    C[i][0] = (-ptrcal[i][1]-25.2-20 * log10(sqrt(pow(xi-xnew[i],2) + pow(yi-ynew[i],2))) / ptrcal[i][0]);
+    B[i] = 1/ptrcal[i][0];
   }
 
  double arr[2][1] = {
