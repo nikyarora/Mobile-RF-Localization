17459861d6fbf169454edef63945d9e3cbb4234a
nikyarora <nikyarora@gmail.com>
2016-08-02 15:40:38 -0700

    changes

--- Arduino/ssLocalizationLibrary/ssLocalizationLib.h
+++ Arduino/ssLocalizationLibrary/ssLocalizationLib.h
@@ -24,7 +24,7 @@ class ssLocalizationLib
 {
   public:
     //ssLocalizationLib();
-    void generateMatrices(float ptrx[xsize], float ptry[ysize], float ptrcal[xsize][num_nodes], float xi, float yi, float A[xsize][num_nodes], float B[xsize][1],float C[xsize][1]);
+    void generateMatrices(float ptrx[xsize], float ptry[ysize], float ptrcal[xsize][NUMBER_OF_NODES], float xi, float yi, float A[xsize][NUMBER_OF_NODES], float B[xsize][1],float C[xsize][1]);
     void add(float arr1[m][1], float arr2[m][1], int m, float finalarr[m][1]);
     void subtract(float arr[m][1], float arr2[m][1], int m, float finalarr[m][1]);
     void multiply(float arr1[m][n], float arr2[n][1], int m, int n, float finalarr[m][1]);
