9b4bbc686bd0cfcb4d3d11df2d204d4828cbacbf
nikyarora <nikyarora@gmail.com>
2016-08-03 13:23:28 -0700

    calculating the inverse of A and multiplying it by rssi-B-C

--- Arduino/allMobileTest/allMobileTest.ino
+++ Arduino/allMobileTest/allMobileTest.ino
@@ -4,7 +4,6 @@
 #include <Time.h>
 #include <MatrixMath.h>
 #include <math.h>
-#include <ssLocalizationLib.h>
 
 // Mobile_Node_Code.pde
 // -*- mode: C++ -*-
@@ -38,13 +37,16 @@ float A[xsize][2];
 float B[xsize][1];
 float C[xsize][1];
 
-//CALIBRATION STUFF
+int m = xsize;
+int n = 2;
+
+//CALIBRATION VALUES
 float x[xsize] = {14, 5, 7};
 float y[ysize] = {9, 12, 11};
 float cal[xsize][NUMBER_OF_NODES - 1] = {
-    {1, 2},
-    {3, 4},
-    {5, 6}
+    {1, 2, 4},
+    {3, 4, 2},
+    {5, 6, 9}
   };
 
 //Tells node whether it has received from all the other nodes
@@ -233,7 +235,29 @@ void loop()
           {
             rssiValues[i][0] = data[i];
           }
-          //xi = pow(A, -1) * (rssiValues - B - C);
+
+          //subtract B and C from the RSSI values
+          for(int i = 0; i < m; i++)
+          {
+            rssiValues[i][0] = rssiValues[i][0] - B[i][0] - C[i][0];
+          }
+
+          float multiplyA[2][3];
+          float newArray[2][2];
+          inverse(A, multiplyA);
+
+          //multiply the inverse of A by rssiValues
+          for(int i = 0; i < 2; i++)
+          {
+            for(int j = 0; j < 1; j++)
+            {
+              newArray[i][j]=0;
+              for(int k = 0; k < 3; k++)
+              {
+                newArray[i][j]= newArray[i][j] + (multiplyA[i][k] * rssiValues[k][j]);
+              }
+            }
+          }
         }
       }
 
@@ -261,9 +285,12 @@ void loop()
 
 /*****************************************************************BROADCAST********************************************************************/
 
-/**
+/************************************************************************************************************
  * This is the broadcasting method for the loop.
- */
+ * It sends one broadcast and then exits the function.
+ * There is a delay at the beginning so the other nodes have time to get into the receive
+ * and don't miss the broadcast.
+ ************************************************************************************************************/
 void broadcast()
 {
   //BROADCAST
@@ -285,11 +312,11 @@ void broadcast()
 
 /*****************************************************************SETUP FUNCTIONS********************************************************************/
  
-/*
+/*************************************************************
  * This is the method that is called by Node 1 in the setup
  * The first node receives from each active broadcasting node
  * and sends an acknowledgement.
- */
+ *************************************************************/
 int receiveSetup()
 {
   if (manager.available())
@@ -450,10 +477,10 @@ int receiveSetup()
    }
 }
 
-/**
+/**************************************************************
  * This is the method called by all the nodes in the setup
  * to broadcast and then receive an acknowledgement.
- */
+ *************************************************************/
 int receiveAcknowledge()
 {
   //BROADCAST
@@ -493,5 +520,178 @@ int receiveAcknowledge()
   else
     Serial.println("sendtoWait failed");
     return 0;
+}
+
+/*****************************************************GENERATE MATRICES and OTHER MATRIX FUNCTIONS*********************************************************/
+
+void generateMatrices(float ptrx[xsize], float ptry[ysize], float ptrcal[xsize][NUMBER_OF_NODES - 1], float xi, float yi, float A[xsize][2], float B[xsize][1],float C[xsize][1])
+{
+  int m = xsize;
+  int n = 2;
+  for(int i = 0; i < m; i++)
+  {
+    for(int j = 0; j < n; j++)
+    {
+      A[i][j] = 0.0;
+    }
+
+    B[i][0] = 0.0;
+    C[i][0] = 0.0;
+  }
+
+  for(int i = 0; i < m; i++)
+  {
+    float linearM[] = {2*(xi-ptrx[i])/(pow(xi-ptrx[i],2)+pow(yi-ptry[i],2)), 2*(yi-ptry[i])/(pow(xi-ptrx[i],2)+pow(yi-ptry[i],2))};
+    for(int j = 0; j < n; j++)
+    {
+      A[i][j] = (-10.0 /ptrcal[i][0]) * linearM[j];
+    }
+    C[i][0] = (-ptrcal[i][1]-25.2-20 * log10(sqrt(pow(xi-ptrx[i],2) + pow(yi-ptry[i],2))) / ptrcal[i][0]);
+    B[i][0] = 1/ptrcal[i][0];
+  }
+
+ float arr[2][1] = {
+    {-xi},
+    {-yi}
+  };
+
+  float finalarr[m][1];
+  
+  for (int i = 0; i < m; i++)
+  {
+    finalarr[i][0] = 0.0;
+    for (int k = 0; k < n; k++)
+    {
+      finalarr[i][0] = finalarr[i][0] + (A[i][k] * arr[k][0]);
+      Serial.println(finalarr[i][0]);
+    }
+  }
+  
+  for (int i = 0; i < m; i++)
+  {
+    C[i][0] = C[i][0] + finalarr[i][0];
+  }
+}
+
+/**************************************
+ * Calculate the pseudoinverse of A
+ *************************************/
+void inverse(float A[][2], float multiplyA[2][3])
+{
+  //TRANSPOSE A
+  float newA[n][m];
+  for(int i = 0; i < m; i++)
+  {
+    for(int j = 0; j < n; j++)
+    {
+      newA[j][i] = A[i][j];
+    }
+  }
+
+  //MULTIPLY A BY A TRANSPOSE
+  float AByATranspose[2][2];
+  for (int i = 0;i < m; i++)
+  {
+    for(int j = 0; j < m; j++)
+    {
+      AByATranspose[i][j]=0;
+      for(int k = 0; k < n ; k++)
+      {
+        AByATranspose[i][j]= AByATranspose[i][j] + (A[i][k]*newA[k][j]);
+      }
+    }
+  }
+
+    //INVERESE OF AByATranspose
+    int pivrow;     // keeps track of current pivot row
+    int k,i,j;      // k: overall index along diagonal; i: row index; j: col index
+    int pivrows[n]; // keeps track of rows swaps to undo at end
+    float tmp;      // used for finding max value and making column swaps
+ 
+    for (k = 0; k < n; k++)
+    {
+        // find pivot row, the row with biggest entry in current column
+        tmp = 0;
+        for (i = k; i < n; i++)
+        {
+            if (abs(AByATranspose[i][k]) >= tmp)   // 'Avoid using other functions inside abs()?'
+            {
+                tmp = abs(AByATranspose[i][k]);
+                pivrow = i;
+            }
+        }
+ 
+        // check for singular matrix
+        if (AByATranspose[pivrow][k] == 0.0f)
+        {
+            Serial.println("Inversion failed due to singular matrix");
+        }
+ 
+        // Execute pivot (row swap) if needed
+        if (pivrow != k)
+        {
+            // swap row k with pivrow
+            for (j = 0; j < n; j++)
+            {
+                tmp = AByATranspose[k][j];
+                AByATranspose[k][j] = AByATranspose[pivrow][j];
+                AByATranspose[pivrow][j] = tmp;
+            }
+        }
+        pivrows[k] = pivrow;    // record row swap (even if no swap happened)
+ 
+        tmp = 1.0f/AByATranspose[k][k];    // invert pivot element
+        AByATranspose[k][k] = 1.0f;        // This element of input matrix becomes result matrix
+ 
+        // Perform row reduction (divide every element by pivot)
+        for (j = 0; j < n; j++)
+        {
+            AByATranspose[k][j] = AByATranspose[k][j]*tmp;
+        }
+ 
+        // Now eliminate all other entries in this column
+        for (i = 0; i < n; i++)
+        {
+            if (i != k)
+            {
+                tmp = AByATranspose[i][k];
+                AByATranspose[i][k] = 0.0f;  // The other place where in matrix becomes result mat
+                for (j = 0; j < n; j++)
+                {
+                    AByATranspose[i][j] = AByATranspose[i][j] - AByATranspose[k][j]*tmp;
+                }
+            }
+        }
+    }
+ 
+    // Done, now need to undo pivot row swaps by doing column swaps in reverse order
+    for (k = n-1; k >= 0; k--)
+    {
+        if (pivrows[k] != k)
+        {
+            for (i = 0; i < n; i++)
+            {
+                tmp = AByATranspose[i][k];
+                AByATranspose[i][k] = AByATranspose[i][pivrows[k]];
+                AByATranspose[i][pivrows[k]] = tmp;
+            }
+        }
+    }
+
+  //MUTLIPLY A TRANSPOSE BY A INVERSE
+  //A[3][2] --> multiplyA[2][3]
+  //newA[2][3]
+  //AByATranspose[2][2]
+  for (int i = 0; i < 2; i++)
+  {
+    for(int j = 0; j < 3; j++)
+    {
+      multiplyA[i][j]=0;
+      for(int k = 0; k < 2; k++)
+      {
+        multiplyA[i][j]= multiplyA[i][j] + (AByATranspose[i][k] * newA[n][j]);  
+      }
+    }
+  }
 }
 
