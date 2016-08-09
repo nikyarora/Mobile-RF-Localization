08dd33cae65caba329acf9e51cf6a27f561149ba
cram9030 <ncramer@ucsc.edu>
2016-08-09 08:34:59 -0700

    Updated Cal_linearize_calculation.py
    This is now representative of the current serial configuration and is
    expecting positions instead of RSSI values.

--- Python/Cal_Linearized_Calculation.py
+++ Python/Cal_Linearized_Calculation.py
@@ -31,80 +31,17 @@ pos[0][1] = yi
 line=[]
 data = "test"
 
-#########################################################################
-#setting up screen
-'''
-wn = turtle.Screen()
-wn.title("RF Network Localization-Multilateral linearization")
-wn.bgpic('grid.gif')
-turtle.setworldcoordinates(-500, -500, 500, 500)
-
-#setting up nodes
-turtle.register_shape("ali.gif")   #Only supports .gif  !!!
-turtle.register_shape("ali.gif")
-
-for i in range (0, len(x_anchor)):
-    station1 = turtle.Turtle()
-    station1.shape("ali.gif")
-    station1.penup()
-
-    station1.setpos((int(x_anchor[i]*100),int(y_anchor[i]*100)))
-    station1.write(station1.pos(), False, align= "center")
-
-mobile_station = turtle.Turtle()
-mobile_station.shape("ali.gif")'''
-
 
 #ser.write(bytes(data, 'UTF-8'))
 ser.write(data.encode())
 stime = time.time()
 while(ser.isOpen()&((time.time()-stime)<10)):
-    time.sleep(.05)
-    C= ser.read(numAnchor + 1)#.decode()
-    time.sleep(.05)
-    if(len(C)>0):
-        #print ("buffer length")
-        #print(len(C))
-        C = C + ser.readline()
+    C = ser.readline()
 
-        #print ("1", ord(C[1]))
-        #print ("2", ord(C[2]))
-        #print ("3", ord(C[3]))
-
-        j=0
-        tempRSSI = numpy.zeros((1,numAnchor),int)
-        #print (len(RSSI))
-        for j in range (0, numAnchor):
-            if ord(C[j+1]) == 0:
-                (C[j+1])=chr(255)
-            tempRSSI[0][j-1]=ord((C[j+1]))
-        #mobile_station.speed(0)
-        RSSI = numpy.append(RSSI,tempRSSI,axis = 0)
-
-        #function to interpret anchor locations and calibrations into matrices
-        [A, B, C] = ssLocalizationLib.generateMatrices(numpy.matrix(x_anchor), numpy.matrix(y_anchor),cal,xi,yi)
-        #print numpy.linalg.pinv(A)
-        #print B
-        #print C
-        #print tempRSSI
-        t_matrix = numpy.dot(numpy.linalg.pinv(A),(tempRSSI - C - B * Brodcast_dB).reshape(nums,1))
-        pos = numpy.append(pos,t_matrix.T,axis = 0)
-        print t_matrix
-
-        '''
-        print("x_m is", t_matrix[0])
-        print("y_m is", t_matrix[1])
+    if len(C) > 0:
+        tempString = C.split(',')
         
-        mobile_station.penup()
-        mobile_station.setpos(t_matrix[0], t_matrix[1])
-        #mobile_station.stamp()
-        mobile_station.write(mobile_station.pos(), False, align= "center")
-        #print(coordinate[0]*100, coordinate[1]*100)
-        time.sleep(2)
-        mobile_station.clear()'''
-        i=0
-        i=i+1
-        #print (i)
+    pos = numpy.append(pos,[tempString[0],tempString[1]],axis = 0)
 
 #wn.mainloop()
 ser.close()
@@ -117,8 +54,3 @@ with open('testat5.csv','wb') as csvfile:
     spamwriter = csv.writer(csvfile, delimiter=',',quotechar='|',quoting=csv.QUOTE_MINIMAL)
     for i in range(0,len(pos)):
         spamwriter.writerow(pos[i][:])
-        
-with open('RSSIat5.csv','wb') as csvfile:
-    spamwriter = csv.writer(csvfile, delimiter=',',quotechar='|',quoting=csv.QUOTE_MINIMAL)
-    for i in range(0,len(RSSI)):
-        spamwriter.writerow(RSSI[i][:])
